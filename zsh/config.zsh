bcb50287c74a3c7586252714754f8a7dd2b05884
Aditya Mukherjee <aditya@adityamukherjee.com>
2013-09-29 01:44:04 +0530

    removed autojump stuff, fixed subl symlink

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -69,9 +69,6 @@ export LESS_TERMCAP_so=$'\E[38;5;246m'    # begin standout-mode - info box
 export LESS_TERMCAP_ue=$'\E[0m'           # end underline
 export LESS_TERMCAP_us=$'\E[04;38;5;146m' # begin underline
 
-# for autojump
-[[ -s /etc/profile.d/autojump.zsh ]] && source /etc/profile.d/autojump.zsh
-
 # vim keybinds in terminal? awwwww yeeeaaahhh
 # bindkey -v
 
