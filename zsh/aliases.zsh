633bc194944500c6fd46a034f6ccf728f1b3a40c
Zach Holman <your@example.com>
2014-09-19 14:02:08 +0530

    added mount statements for ubuntu

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,10 +1,18 @@
 alias reload!='. ~/.zshrc'
 
-hist() { 
-	if [ -z "$*" ]; then 
+hist() {
+	if [ -z "$*" ]; then
 		history 1;
 	else
 		history 1 | egrep "$@";
 	fi;
 }
 
+alias mount_dph="ssh-add ~/Dropbox/funhouse/funhouse.pem;\
+				 sshfs -o idmap=user ubuntu@area51.dotaprohub.com:/home/ubuntu/fhweb/api ~/dev/Dotaprohub/api;\
+				 sshfs -o idmap=user ubuntu@area51.dotaprohub.com:/home/ubuntu/fhweb/frontend ~/dev/Dotaprohub/frontend;\
+				 sshfs -o idmap=user ubuntu@area51.dotaprohub.com:/home/ubuntu/dphnode ~/dev/Dotaprohub/dphnode;"
+
+alias unmount_dph=" sudo fusermount -z -u ~/dev/Dotaprohub/api;\
+					sudo fusermount -z -u ~/dev/Dotaprohub/frontend;\
+					sudo fusermount -z -u ~/dev/Dotaprohub/dphnode;"
